c6797a0eeea42993b138ee0eb17c78fc4c3e8700
willymariino <wg.mariino@gmail.com>
2026-03-16 19:29:17 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,37 @@
+console.log("JS Loading");
+
+//DATASET
+const cars = [
+  {
+    name: "Luigi",
+    trackColor: "red",
+    id: "car-1",
+    increment: 0,
+    currentPosition: 0,
+    time: 0,
+  },
+  {
+    name: "Marco",
+    trackColor: "brown",
+    id: "car-2",
+    increment: 0,
+    currentPosition: 0,
+    time: 0,
+  },
+  {
+    name: "Alfredo",
+    trackColor: "olive",
+    id: "car-3",
+    increment: 0,
+    currentPosition: 0,
+    time: 0,
+  },
+  {
+    name: "Alessandro",
+    trackColor: "navy",
+    id: "car-4",
+    increment: 0,
+    currentPosition: 0,
+    time: 0,
+  },
+];
